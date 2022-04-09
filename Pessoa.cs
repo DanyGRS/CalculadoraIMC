f0190268dec0de39a79b483ffef8fca608c1c891
Danyelle Santana <dany.san120404@gmail.com>
2022-04-09 00:09:40 -0300

    Adicionando a classe Pessoa

--- Pessoa.cs
+++ Pessoa.cs
@@ -3,7 +3,7 @@ public class Pessoa
     public string Nome { get; set; }    
     public double Altura { get; set; }
     public double Peso { get; set; }
-    public double Imc { get => Peso / Altura * Altura; }
+    public double Imc { get => Math.Round(((Peso) / (Altura * Altura)), 2); }
 
     public Pessoa(string nome, double altura, double peso)
     {
@@ -14,53 +14,38 @@ public class Pessoa
 
     public string CalculoIMC ()
     {
-        if (Imc < 17)
-        {
-            return "Muito abaixo do peso";
-        }
-        else if (Imc >= 17.00 && Imc < 18.50)
-        {
-            return "Abaixo do peso";
-        }
-        else if (Imc >= 18.50 && Imc < 25.00)
-        {
-            return "Peso normal";
-        }
-        else if (Imc >= 25.00 && Imc < 30.00)
-        {
-            return "Acima do peso";
-        }
-        else if (Imc >= 30.00 && Imc < 35.00)
-        {
-            return "Obesidade I";
-        }
-        else if (Imc >= 35.00 && Imc < 40.00)
-        {
-            return "Obesidade II(severa)";
-        }
-        else if(Imc > 40.00)
-        {
-            return "Obesidade III(mórbida)";
-        }
-        return -1;
+        if (Imc < 17) return "Muito abaixo do peso";
+
+        else if (Imc >= 17.00 && Imc < 18.50) return "Abaixo do peso";
+
+        else if (Imc >= 18.50 && Imc < 25.00) return "Peso normal";
+      
+        else if (Imc >= 25.00 && Imc < 30.00) return "Acima do peso";
+   
+        else if (Imc >= 30.00 && Imc < 35.00) return "Obesidade I";
+  
+        else if (Imc >= 35.00 && Imc < 40.00) return "Obesidade II(severa)";
+    
+        else if (Imc > 40.00) return "Obesidade III(mórbida)";
+
+        return "-1";
     }
-
-    public double PesoIdeal(double imc)
+    
+    public string PesoIdeal()
     {
-        if(imc < 18.50)
+        if(Imc < 18.50)
         {
-            double pesoEssencial = Math.Round(18.50 - imc, 2);
+            double pesoEssencial = Math.Round((18.50 - Imc), 2);
 
-            return "Você precisa ganhar Kg: " + pesoEssencial;
+            return "Você precisa ganhar: " + (pesoEssencial) + "Kg";
         }
 
-        else if(imc >= 25)
+        else if(Imc >= 25)
         {
-            double pesoEssencial = Math.Round(imc - 24.99, 2);
+            double pesoEssencial = Math.Round((Imc - 24.99), 2);
 
-            return "Você precisa perder Kg" + pesoEssencial;
+            return "Você precisa perder:" + (pesoEssencial) +"Kg";
         }
-    return -1;  
-
+    return "Você já está com o peso ideal!!!";  
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ namespace CalculadoraIMC
         static void Main(string[] args)
         {
             Console.WriteLine("Qual é o seu nome?");
-            string nome = Console.ReadLine();
+            string Nome = Console.ReadLine();
 
-            Console.WriteLine("Qual é a sua altura em metros? Ex. 1,65");
-            double altura = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Qual é a sua altura em metros? Ex. 1.65");
+            double Altura = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Qual o seu peso em Kg? Ex: 70,3");
-            double peso = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Qual o seu peso em Kg? Ex: 70.3");
+            double Peso = Convert.ToSingle(Console.ReadLine());
 
-            Pessoa p1 = new Pessoa(nome, peso, altura);
+            Pessoa p1 = new Pessoa(Nome, Altura, Peso);    
 
-            Console.WriteLine("Seu IMC é {0}", p1.CalculoIMC);
-
-             
+            Console.WriteLine("Nome: {2}, Peso: {0}Kg, Altura: {1}m", p1.Peso, Math.Round(p1.Altura, 2) , p1.Nome);
+            Console.WriteLine("Seu IMC é {0} e você está {1}", p1.Imc, p1.CalculoIMC()); 
+            Console.WriteLine(p1.PesoIdeal());
         }
     }
 }
